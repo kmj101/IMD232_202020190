26bf3eaccff516efe4153d5347145e609fed7b2a
kmj101 <127805445+kmj101@users.noreply.github.com>
2023-10-16 07:36:14 +0900

    cfe

--- src/w4/assignment/step2/script/Mover.js
+++ src/w4/assignment/step2/script/Mover.js
@@ -1,18 +1,28 @@
 class Mover {
   constructor(x, y, mass) {
-    this.pos;
-    this.vel;
-    this.acc;
-    this.mass;
-    this.rad;
-    this.isHover;
-    this.isDragging;
-    this.draggingOffset;
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.accDisplay = createVector(0, 0);
+    this.mass = mass;
+    this.radius = this.mass ** 0.5 * 10;
+    this.draggingOffset = createVector();
+    this.isHover = false;
+    this.isDragging = false;
   }
 
-  applyForce(force) {}
+  applyForce(force) {
+    let forceDividedByMass = createVector(force.x, force.y);
+    forceDividedByMass.div(this.mass);
+    this.acc.add(forceDividedByMass);
+  }
 
-  update() {}
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.accDisplay.set(this.acc);
+    this.acc.mult(0);
+  }
 
   edgeBounce() {
     const bounce = -0.7;
@@ -29,24 +39,66 @@ class Mover {
     }
   }
 
+  contactEdge() {
+    if (this.pos.y >= height - 1 - this.radius - 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  checkEdges() {
+    const bounce = -0.9;
+    if (this.pos.x < 0 + this.radius) {
+      this.pos.x -= 0 + this.radius;
+      this.pos.x *= -1;
+      this.pos.x += 0 + this.radius;
+      this.vel.x *= bounce;
+    } else if (this.pos.x > width - 1 - this.radius) {
+      this.pos.x -= width - 1 - this.radius;
+      this.pos.x *= -1;
+      this.pos.x += width - 1 - this.radius;
+      this.vel.x *= bounce;
+    }
+    if (this.pos.y > height - 1 - this.radius) {
+      this.pos.y -= height - 1 - this.radius;
+      this.pos.y *= -1;
+      this.pos.y += height - 1 - this.radius;
+      this.vel.y *= bounce;
+    }
+  }
+
   display() {
     noStroke();
     fill(0);
-    ellipse(this.pos.x, this.pos.y, 2 * this.rad);
+    ellipse(this.pos.x, this.pos.y, 3 * this.radius);
   }
 
   mouseMoved(mX, mY) {
     this.isHover =
-      (this.pos.x - mX) ** 2 + (this.pos.y - mY) ** 2 <= this.rad ** 2;
+      (this.ballPos.x - mX) ** 2 + (this.ballPos.y - mY) ** 2 <= this.rad ** 2;
   }
 
   mousePressed(mX, mY) {
     if (this.isHover) {
+      this.isDragging = true;
+      this.angleAcc = 0;
+      this.angleVel = 0;
+      this.draggingOffset.set(mX - this.ballPos.x, mY - this.ballPos.y);
     }
   }
 
   mouseDragged(mX, mY) {
     if (this.isDragging) {
+      const ballShouldBe = createVector(
+        mX - this.draggingOffset.x,
+        mY - this.draggingOffset.y
+      );
+      const angle = atan2(
+        ballShouldBe.y - this.pos.y,
+        ballShouldBe.x - this.pos.x
+      );
+      this.angle = angle;
     }
   }
 
@@ -54,3 +106,5 @@ class Mover {
     this.isDragging = false;
   }
 }
+
+displayVectors();
--- src/w4/assignment/step2/script/sketch.js
+++ src/w4/assignment/step2/script/sketch.js
@@ -1,35 +1,56 @@
-let mover;
+let moverA;
 let gravity;
 let mVec;
 let pMVec;
 
 function setup() {
-  setCanvasContainer('canvas', 1, 1, true);
-
-  mover = new Mover(width / 2, height / 2, 100);
-  gravity = createVector(0, 0.5);
-
-  mVec = createVector();
-  pMVec = createVector();
-
+  setCanvasContainer('canvas', 3, 2, true);
   background(255);
+  moverA = new Mover(width / 3, height / 2, 10);
+  gravity = createVector(0, 0.1);
+  wind = createVector(1, 0);
+  mVec = createVector(mouseX, mouseY);
+  pMVec = createVector(pmouseX, pmouseY);
 }
 
 function draw() {
-  const force = p5.Vector.mult(gravity, mover.mass);
-
   background(255);
+
+  let gravityA = createVector(gravity.x, gravity.y);
+  gravityA.mult(moverA.mass);
+  moverA.applyForce(gravityA);
+  if (mouseIsPressed && isMouseInsideCanvas()) {
+    moverA.applyForce(moverA);
+  }
+  if (mouseDragged) {
+    moverA.applyForce(mVec);
+  }
+  if (moverA.contactEdge()) {
+    let c = 0.01;
+    let friction = moverA.vel.copy();
+    friction.mult(-1);
+    friction.mult(c);
+    moverA.applyForce(friction);
+  }
+  moverA.update();
+  moverA.checkEdges();
+  moverA.display();
 }
 
-function mouseMoved() {}
+function mouseMoved() {
+  moverA.mouseMoved(mouseX, mouseY);
+}
 
-function mousePressed() {}
+function mousePressed() {
+  moverA.mousePressed(mouseX, mouseY);
+}
 
-function mouseDragged() {}
+function mouseDragged() {
+  moverA.mouseDragged(mouseX, mouseY);
+}
 
 function mouseReleased() {
   pMVec.set(pmouseX, pmouseY);
   mVec.set(mouseX, mouseY);
-
-  mover.applyForce(throwingForce);
+  moverA.applyForce(throwingForce);
 }
