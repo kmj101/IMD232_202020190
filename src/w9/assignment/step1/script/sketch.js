c0ed14bc24915023b8a7fafebcd738ffd9311486
kmj101 <127805445+kmj101@users.noreply.github.com>
2023-11-20 15:27:25 +0900

    ㅇㄹㄴㅁㅇ

--- src/w9/assignment/step1/script/sketch.js
+++ src/w9/assignment/step1/script/sketch.js
@@ -1,15 +1,4 @@
-const {
-  Engine,
-  Render,
-  Runner,
-  Body,
-  Composite,
-  Composites,
-  Constraint,
-  MouseConstraint,
-  Mouse,
-  Bodies,
-} = Matter;
+const { Engine, Render, World, Bodies } = Matter;
 
 // create engine
 const engine = Engine.create();
@@ -30,64 +19,19 @@ const render = Render.create({
 
 Render.run(render);
 
-// create runner
-const runner = Runner.create();
-Runner.run(runner, engine);
-
-// add bodies
-const group = Body.nextGroup(true);
-
-// First chain (pink)
-const ropeA = Composites.stack(100, 50, 8, 1, 10, 10, (x, y) => {
-  return Bodies.rectangle(x, y, 50, 20, {
-    collisionFilter: { group: group },
-    render: { fillStyle: '#FF1493' }, // Pink color
-  });
-});
-
-Composites.chain(ropeA, 0.5, 0, -0.5, 0, {
-  stiffness: 0.8,
-  length: 2,
-  render: { type: 'line' },
-});
-
-Composite.add(
-  ropeA,
-  Constraint.create({
-    bodyB: ropeA.bodies[0],
-    pointB: { x: -25, y: 0 },
-    pointA: {
-      x: ropeA.bodies[0].position.x,
-      y: ropeA.bodies[0].position.y,
-    },
-    stiffness: 0.5,
-  })
-);
-
-Composite.add(world, [
-  ropeA,
-  Bodies.rectangle(400, 600, 1200, 50.5, { isStatic: true }),
-]);
+// add a rectangle
+const rectangle = Bodies.rectangle(400, 300, 80, 40, { fillStyle: '#FF1493' }); // Pink color
+World.add(world, rectangle);
 
 // add mouse control
-const mouse = Mouse.create(render.canvas);
-const mouseConstraint = MouseConstraint.create(engine, {
-  mouse: mouse,
-  constraint: {
-    stiffness: 0.2,
-    render: {
-      visible: false,
-    },
-  },
+const mouseConstraint = Matter.MouseConstraint.create(engine, {
+  element: render.canvas,
 });
 
-Composite.add(world, mouseConstraint);
-
-// keep the mouse in sync with rendering
-render.mouse = mouse;
+World.add(world, mouseConstraint);
 
 // fit the render viewport to the scene
 Render.lookAt(render, {
   min: { x: 0, y: 0 },
-  max: { x: 700, y: 600 },
+  max: { x: 800, y: 600 },
 });
